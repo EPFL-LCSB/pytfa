acca42b073feb8680b615398f936c1c431f644f0
Pierre Salvy <pierre.salvy@outlook.com>
2017-08-16 16:12:35 +0200

    REQ: Adding optlang to the requirements in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 setup(name='pytfa',
       version='1.1.0',
       author='pyTFA team',
-      install_requires=['cobra>0.6,<=0.8.1','bokeh>=0.12.1'],
+      install_requires=['cobra>0.6,<=0.8.1','bokeh>=0.12.1','optlang'],
       py_modules=['pytfa'],
       python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
       description='pyTFA adds thermodynamic constraints for Flux-Based Analysis of reactions'
