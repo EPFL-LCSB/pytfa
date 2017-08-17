a0ef44d35a02b754ac242dd07acda86f7888ba22
Pierre Salvy <pierre.salvy@outlook.com>
2017-08-17 08:09:55 +0200

    MNT: cleaning repo for Travis/Codecov

--- setup.py
+++ setup.py
@@ -7,10 +7,13 @@
 
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name='pytfa',
       version='1.1.0',
       author='pyTFA team',
-      install_requires=['cobra>0.6,<=0.8.1','bokeh>=0.12.1','optlang'],
+      install_requires=requirements,
       py_modules=['pytfa'],
       python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
       description='pyTFA adds thermodynamic constraints for Flux-Based Analysis of reactions'
