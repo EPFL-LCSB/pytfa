d214b122ce7e4451fb6997452d4b4cae5e39d179
Pierre Salvy <pierre.salvy@outlook.com>
2017-08-17 10:50:14 +0200

    VER: prepare for pypi release

--- setup.py
+++ setup.py
@@ -13,8 +13,12 @@ with open('requirements.txt') as f:
 setup(name='pytfa',
       version='0.6.0',
       author='pyTFA team',
+      author_email='softwares.lcsb@epfl.ch',
+      url='https://github.com/EPFL-LCSB/pytfa/',
+      download_url='https://github.com/EPFL-LCSB/pytfa/archive/0.6.0-a1.tar.gz',
       install_requires=requirements,
       py_modules=['pytfa'],
       python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
-      description='pyTFA adds thermodynamic constraints for Flux-Based Analysis of reactions'
+      description='pyTFA adds thermodynamic constraints for Flux-Based Analysis of reactions',
+      keywords=['pytfa','tfa','thermodynamics','flux analysis'],
      )
