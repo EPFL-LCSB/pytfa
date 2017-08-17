5202eb904b8ce8ff70542edb9de0995c9c66a1a8
Pierre Salvy <pierre.salvy@outlook.com>
2017-08-17 11:27:10 +0200

    FIX: setup.py compatible with pypi

--- setup.py
+++ setup.py
@@ -6,9 +6,15 @@
 """
 
 from setuptools import setup
+from pip.req import parse_requirements
+from pip.download import PipSession
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+def read_requirements():
+    '''parses requirements from requirements.txt'''
+    reqs_path = os.path.join(__location__, 'requirements.txt')
+    install_reqs = parse_requirements(reqs_path, session=PipSession())
+    reqs = [str(ir.req) for ir in install_reqs]
+    return reqs
 
 setup(name='pytfa',
       version='0.6.0',
@@ -16,7 +22,7 @@ setup(name='pytfa',
       author_email='softwares.lcsb@epfl.ch',
       url='https://github.com/EPFL-LCSB/pytfa/',
       download_url='https://github.com/EPFL-LCSB/pytfa/archive/0.6.0-a1.tar.gz',
-      install_requires=requirements,
+      install_requires=read_requirements(),
       py_modules=['pytfa'],
       python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
       description='pyTFA adds thermodynamic constraints for Flux-Based Analysis of reactions',
