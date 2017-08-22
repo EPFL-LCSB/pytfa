7fa4cedd76186d5cef7a857e82b1bd87d462d623
Pierre Salvy <pierre.salvy@outlook.com>
2017-08-22 13:50:56 +0200

    ENH: Gurobi floating license compatibility

--- docker/utils/activate_gurobi.sh
+++ docker/utils/activate_gurobi.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
-# Script to activate gurobi - remember to add your license key!\\
-# Rename to activate_gurobi.sh
-# Gurobi does not allow to install a single-user license on root
 # Choose the GHOME variable depending on your gurobi version
 
-export LICENSE_KEY=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
 export GHOME_702="/opt/gurobi702/linux64"
 export GHOME_750="/opt/gurobi750/linux64"
 export GHOME=$GHOME_702
 
 if [ -d $GHOME ]; then \
-	$GHOME/bin/grbgetkey $LICENSE_KEY	&& \
+	#$GHOME/bin/grbgetkey $LICENSE_KEY	&& \
 
 	# Export path
 	echo "export GUROBI_HOME=${GHOME}" >> ~/.bashrc			&&	\
