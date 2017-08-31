c6fa90faefde0909c8f1f314285f6f2b4b97090f
Pierre Salvy <pierre.salvy@epfl.ch>
2017-08-31 17:33:18 +0200

    TST: decentralized tests, made them cleaner with only one tfa call

---
+++ tests/settings.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+.. module:: pytfa
+   :platform: Unix, Windows
+   :synopsis: Thermodynamics-based Flux Analysis
+
+.. moduleauthor:: pyTFA team
+
+Initialization of globals for tests
+
+"""
+import os
+import pytfa.io
+
+############
+# SETTINGS #
+############
+
+# Objective value of the Matlab solution
+objective_value = 0.810997250260066
+
+######## End of Settings ########
+
+
+this_directory = os.path.dirname(os.path.realpath(__file__))
+
+# Load the thermo database
+thermo_data = pytfa.io.load_thermoDB \
+    (this_directory \
+                                     + '/../data/thermo_data.thermodb')
+
+# Load the cobra_model
+cobra_model = pytfa.io.import_matlab_model \
+    (this_directory \
+                                     + '/../models/small_ecoli.mat')
+
+# Make your computations on it
+tmodel = pytfa.ThermoModel(thermo_data, cobra_model)
+tmodel.solver = 'optlang-glpk'
+tmodel.prepare()
+tmodel.convert()
---
+++ tests/test_io.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+.. module:: pytfa
+   :platform: Unix, Windows
+   :synopsis: Thermodynamics-based Flux Analysis
+
+.. moduleauthor:: pyTFA team
+
+Tests the I/O functionalities of pytfa
+
+"""
+
+import os
+
+from settings import cobra_model, tmodel
+
+#############
+#    I/O    #
+#############
+
+def test_write_mat():
+    from pytfa.io.base import write_matlab_model
+    write_matlab_model(tmodel, 'tmp.mat')
+    os.remove('tmp.mat')
+
+def test_read_write_lexicon():
+    from pytfa.io.enrichment import write_lexicon, read_lexicon, annotate_from_lexicon
+    fname = 'tmp.lex.csv'
+    write_lexicon(tmodel,fname)
+    lex = read_lexicon(fname)
+    annotate_from_lexicon(cobra_model,lex)
+    os.remove(fname)
+
+def test_read_write_compdata():
+    from pytfa.io.enrichment import write_compartment_data, read_compartment_data, apply_compartment_data
+    fname = 'tmp.comp.json'
+    write_compartment_data(tmodel,fname)
+    cd = read_compartment_data(fname)
+    apply_compartment_data(cobra_model,fname)
+    os.remove(fname)
