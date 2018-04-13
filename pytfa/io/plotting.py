6407e62ca11dd243a6cd981811db1dc2368fcc84
Pierre Salvy <pierre.salvy@epfl.ch>
2018-04-13 15:41:23 +0200

    ENH: Paper plot

--- pytfa/io/plotting.py
+++ pytfa/io/plotting.py
@@ -53,7 +53,7 @@ def plot_fva_tva_comparison(fva,tva):
 
     plot.yaxis.ticker = [x for x in range(len(tva))]
 
-    show(plot)
+    return plot
 
 def plot_thermo_displacement_histogram(tmodel,solution = None):
     """
@@ -82,7 +82,7 @@ def plot_thermo_displacement_histogram(tmodel,solution = None):
     p1.yaxis.axis_label = 'count'
     p1.title = "Histogram: Thermodynamic displacement"
 
-    show(p1)
+    return p1
 
 def plot_histogram(values, **kwargs):
     """
