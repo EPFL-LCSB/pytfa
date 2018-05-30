cb1b557820c1b462b60697e611550ff797fe54ff
Pierre Salvy <pierre.salvy@epfl.ch>
2018-05-30 13:52:22 +0200

    ENH: Gurobi 8.0.0 default support

--- docker/utils/install_gurobi.sh
+++ docker/utils/install_gurobi.sh
@@ -3,13 +3,21 @@
 
 echo "Installing Gurobi"
 
-if [ -f /solvers/gurobi7.5.2_linux64.tar.gz ]; then \
-	cp /solvers/gurobi7.5.2_linux64.tar.gz /opt	&&	\
+if [ -f /solvers/gurobi8.0.0_linux64.tar.gz ]; then \
+	cp /solvers/gurobi8.0.0_linux64.tar.gz /opt	&&	\
 	cd /opt						&&	\
-	tar xvfz gurobi7.5.2_linux64.tar.gz		&&	\
-	cd /opt/gurobi752/linux64 			&& 	\
+	tar xvfz gurobi8.0.0_linux64.tar.gz		&&	\
+	cd /opt/gurobi800/linux64 			&& 	\
 	python3 setup.py install			&&	\
-	rm /opt/gurobi7.5.2_linux64.tar.gz; fi
+	rm /opt/gurobi8.0.0_linux64.tar.gz; fi
+
+#if [ -f /solvers/gurobi7.5.2_linux64.tar.gz ]; then \
+#	cp /solvers/gurobi7.5.2_linux64.tar.gz /opt	&&	\
+#	cd /opt						&&	\
+#	tar xvfz gurobi7.5.2_linux64.tar.gz		&&	\
+#	cd /opt/gurobi752/linux64 			&& 	\
+#	python3 setup.py install			&&	\
+#	rm /opt/gurobi7.5.2_linux64.tar.gz; fi
 	
 
 #if [ -f /solvers/gurobi7.0.2_linux64.tar.gz ]; then \
