2bf95a0f8ac522837f67c1306738a1958e44c776
Benjamin Mouscadet <benjamin.mouscadet@supelec.fr>
2018-11-27 15:19:01 +0100

    ENH : BBB now user-defined. Improved loading of core/ncore reactions/metabolites

--- pytfa/lumpgem/lumpgem.py
+++ pytfa/lumpgem/lumpgem.py
@@ -7,10 +7,10 @@ import errno
 from io.base import import_matlab_model, load_thermoDB
 
 from optim.variables import BinaryVariable, DeltaG, DeltaGstd, ThermoDisplacement
-from analysis import  variability_analysis,           \
-                            apply_reaction_variability,     \
-                            apply_generic_variability,       \
-                            apply_directionality
+from analysis import    variability_analysis,\
+                        apply_reaction_variability,\
+                        apply_generic_variability,\
+                        apply_directionality
 
 from cobra.flux_analysis.variability import flux_variability_analysis
 
@@ -25,12 +25,17 @@ class LumpGEM:
     """
     A class encapsulating the LumpGEM algorithm
     """
-    def __init__(self, GEM, core_subsystems, carbon_uptake, thermo_data_path):
+    def __init__(self, GEM, biomass_rxns, core_subsystems, carbon_uptake, thermo_data_path):
         """
         : param GEM: the GEM 
         : type GEM: cobra model
+
+        : param biomass_rxns: list of biomass reactions
+        : type biomass_rxns: [GEM.biomass_rxn.id]
+
         : param core_subsystems: list of Core subsystems
         : type core_subsytems: [[model.reactions]]
+
         : param carbon_intake: the amount of carbon atoms the cell intakes from its surrounding
         : type carbon_intake: float
 
@@ -41,23 +46,22 @@ class LumpGEM:
         self._GEM = GEM
 
         # Extracting all reactions that lead to BBB
-        # TODO BBB user-defined
-        self._rBBB = [rxn for rxn in GEM.reactions if "Biomass" in rxn.id]
+        self._rBBB = set([rxn for rxn in GEM.reactions if rxn.id in biomass_rxns])
 
         # Set containing every core reaction
         self._rcore = set([])
+        # Set containing every core metabolite
+        self._mcore = set([])
         for subsystem in core_subsystems:
             for rxn in subsystem:
+                # Add rxn to core reactions
                 self._rcore.add(rxn)
-
+                # Add involved metabolites to core metabolites
+                for met in rxn.metabolites:
+                    self._mcore.add(met)
+ 
         # Non core reactions
-        self._rncore = [rxn for rxn in GEM.reactions if not (rxn in self._rcore or rxn in self._rBBB)]
-
-        # Core metabolites 
-        self._mcore = set([])
-        for rxn in self._rcore:
-            for met in rxn.metabolites:
-                self._mcore.add(met)
+        self._rncore = set([rxn for rxn in GEM.reactions if not (rxn in self._rcore or rxn in self._rBBB)])
 
         # Carbon uptake
         self._C_uptake = carbon_uptake
@@ -85,7 +89,9 @@ class LumpGEM:
         """
         for rxn in self._rncore:
             # rxn contrained according to the carbon uptake
-            rxn_const = self._model.problem.Constraint(rxn.forward_variable + rxn.reverse_variable + self._C_uptake*self._bin_vars[rxn], ub=self._C_uptake)
+            rxn_const = self._model.problem.Constraint( rxn.forward_variable +
+                                                        rxn.reverse_variable + 
+                                                        self._C_uptake * self._bin_vars[rxn], ub=self._C_uptake)
             self._model.add_cons_vars(rxn_const)
 
     def apply_thermo_constraints(self):
