1c75ee231a99fecc9721c5da834935d0fee076e0
Benjamin Mouscadet <benjamin.mouscadet@supelec.fr>
2018-11-27 15:07:40 +0100

    FIX : fixed minor suntax errors. Added TODO flags

--- pytfa/lumpgem/lumpgem.py
+++ pytfa/lumpgem/lumpgem.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-.. module:: lumpgem
-   :platform: Unix, Windows
-   :synopsis: LumpGEM Algorithm
-
-.. moduleauthor:: pyTFA team
-
-Model class
-"""
-
 import os
 import errno
-import pytfa
 
-from pytfa.io import import_matlab_model, load_thermoDB
+from io.base import import_matlab_model, load_thermoDB
 
-from pytfa.optim.variables import DeltaG,DeltaGstd,ThermoDisplacement
-from pytfa.analysis import  variability_analysis,           \
+from optim.variables import BinaryVariable, DeltaG, DeltaGstd, ThermoDisplacement
+from analysis import  variability_analysis,           \
                             apply_reaction_variability,     \
                             apply_generic_variability,       \
                             apply_directionality
@@ -31,16 +20,17 @@ CPLEX = 'optlang-cplex'
 GUROBI = 'optlang-gurobi'
 GLPK = 'optlang-glpk'
 
-class LumpGEM()
+
+class LumpGEM:
     """
     A class encapsulating the LumpGEM algorithm
     """
-    def __init__(self, GEM, rcore, carbon_intake, thermo_data_path):
+    def __init__(self, GEM, core_subsystems, carbon_uptake, thermo_data_path):
         """
         : param GEM: the GEM 
         : type GEM: cobra model
-        : param core: list of Core reactions
-        : type core: model.reactions
+        : param core_subsystems: list of Core subsystems
+        : type core_subsytems: [[model.reactions]]
         : param carbon_intake: the amount of carbon atoms the cell intakes from its surrounding
         : type carbon_intake: float
 
@@ -51,58 +41,61 @@ class LumpGEM()
         self._GEM = GEM
 
         # Extracting all reactions that lead to BBB
+        # TODO BBB user-defined
         self._rBBB = [rxn for rxn in GEM.reactions if "Biomass" in rxn.id]
-        # Core reactions
-        self._rcore = rcore
-        #Core metabolites --- Is it true ? Or must mcore be user-defined ?
-        self.mcore = [met for rxn in self._rcore for met in rxn.metabolites]
-        # Non core reactions
-        self._rncore = [rxn for rxn in _GEM.reactions if not (rxn in _rcore or rxn in _rBBB)]
 
-        # Carbon intake
-        self._C_intake = carbon_intake
+        # Set containing every core reaction
+        self._rcore = set([])
+        for subsystem in core_subsystems:
+            for rxn in subsystem:
+                self._rcore.add(rxn)
+
+        # Non core reactions
+        self._rncore = [rxn for rxn in GEM.reactions if not (rxn in self._rcore or rxn in self._rBBB)]
+
+        # Core metabolites 
+        self._mcore = set([])
+        for rxn in self._rcore:
+            for met in rxn.metabolites:
+                self._mcore.add(met)
+
+        # Carbon uptake
+        self._C_uptake = carbon_uptake
 
         # Load reactions DB
-        self._thermo_data = load_thermo_DB(thermo_data_path)
+        self._thermo_data = load_thermoDB(thermo_data_path)
 
-
-    def build_new_model():
+    def build_new_model(self):
         """
-        TODO : Generate a new GEM model which will be optimized
+        
         """
-        # TODO
-        self._model = Model()
+        # TODO : Generate a new GEM model which will be optimized
+        self._model = None
 
-
-    def generate_binary_variables():
+    def generate_binary_variables(self):
         """
         Generate binary variables for each non-core reaction
         """
-        self._bin_vars = {rxn : Variable(name=rxn.id, type='binary') for rxn in _rncore}
+        # TODO Check the correct construction of variables
+        self._bin_vars = {rxn: BinaryVariable(name=rxn.id, type='binary') for rxn in self._rncore}
 
-    
-    def generate_constraints():
+    def generate_constraints(self):
         """
         Generate carbon intake related constraints for each non-core reaction
         """
-        constraints = []
         for rxn in self._rncore:
-            rxn_const = Constraint(rxn.forward_variable + rxn.reverse_variable + _C_intake*_bin_vars[rxn], ub=_C_intake)
-            constraints.append(rxn_const)
+            # rxn contrained according to the carbon uptake
+            rxn_const = self._model.problem.Constraint(rxn.forward_variable + rxn.reverse_variable + self._C_uptake*self._bin_vars[rxn], ub=self._C_uptake)
+            self._model.add_cons_vars(rxn_const)
 
-        self._model.add(constraints)
-
-
-    def apply_thermo_constraints():
+    def apply_thermo_constraints(self):
         """
         Apply thermodynamics constraints defined in thermoDB to Mcore & Rcore
         """
         # To apply the thermodynamics constraints to Rcore & Mcore only, we will remove every 
         # non core element from self._thermo_data
 
-        # How to extract some particular metabolites / reactions from this DB ? 
-        # Hard to understand its structure
-        #core_thermo_data = process(self._thermo_data)
+        # TODO flags to activate/deactivate thermopt
+        # core_thermo_data = process(self._thermo_data)
 
-       mytfa = pytfa.ThermoModel(core_thermo_data, self._model) 
-
+        # mytfa = ThermoModel(core_thermo_data, self._model) 
