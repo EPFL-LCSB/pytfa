6483ffbdc9c5d283265195747bd0398fce9f0d8d
Benjamin Mouscadet <benjamin.mouscadet@supelec.fr>
2019-02-13 19:08:00 +0100

    ENH : proper generation of binary variables and flux constraints

--- pytfa/lumpgem/lumpgem.py
+++ pytfa/lumpgem/lumpgem.py
@@ -37,10 +37,12 @@ class LumpGEM:
         """
 
         # Load the GEM through the appropriate cobra loading function (based on path suffix)
-        self._model = self._load_model(path_to_model)
+        model = self._load_model(path_to_model)
+        # Build thermo model
+        self._tfa_model = self._apply_thermo_constraints(thermo_data_path, model)
 
         # Extracting all reactions that lead to BBB
-        self._rBBB = set([rxn for rxn in self._model.reactions if rxn.id in biomass_rxns])
+        self._rBBB = set([rxn for rxn in self._tfa_model.reactions if rxn.id in biomass_rxns])
 
         # Set containing every core reaction
         self._rcore = set([])
@@ -55,7 +57,7 @@ class LumpGEM:
                     self._mcore.add(met)
 
         # Non core reactions
-        self._rncore = set([rxn for rxn in self._model.reactions if not (rxn in self._rcore or rxn in self._rBBB)])
+        self._rncore = set([rxn for rxn in self._tfa_model.reactions if not (rxn in self._rcore or rxn in self._rBBB)])
 
         # Carbon uptake
         self._C_uptake = carbon_uptake
@@ -65,11 +67,9 @@ class LumpGEM:
         # TODO : solver choice
         self._solver = 'optlang-cplex'
 
-        # Build thermo model
-        self._tfa_model = self._apply_thermo_constraints(thermo_data_path, self._model)
-
         self._bin_vars = self._generate_binary_variables()
         self._generate_constraints()
+        self._generate_objective()
 
     def _load_model(self, path_to_model):
         # Matlab
@@ -88,38 +88,6 @@ class LumpGEM:
         if path_to_model[-4:] == ".xml":
             return read_sbml_model(path_to_model)
 
-    def _generate_constraints(self):
-        """
-        Generate carbon intake related constraints for each non-core reaction and 
-        growth rate related constraints for each BBB reaction
-        """
-        # Carbon intake constraints
-        for rxn in self._rncore:
-            # rxn constrained according to the carbon uptake
-            rxn_const = self._tfa_model.problem.Constraint(rxn.forward_variable +
-                                                           rxn.reverse_variable +
-                                                           self._C_uptake * self._bin_vars[rxn], ub=self._C_uptake)
-            self._tfa_model.add_cons_vars(rxn_const)
-
-        # Growth rate constraints
-        for bio_rxn in self._rBBB:
-            bio_rxn.lower_bound = self._growth_rate
-
-    def _generate_objective(self):
-        """
-        Generate and add the maximization objective
-        """
-        # List of all binary variables
-        # TODO Check the correct construction of variables
-        bin_vars = [BinaryVariable(rxn.id, self._tfa_model) for rxn in self._rncore]
-        # Add all binary variables to the model
-        self._tfa_model.add_cons_vars(bin_vars)
-
-        # Sum of binary variables to be maximized
-        objective_sum = sum(bin_vars)
-        # Set the sum as the objective function
-        self._tfa_model.objective = self._tfa_model.problem.Objective(objective_sum, direction='max')
-
     def _apply_thermo_constraints(self, thermo_data_path, cobra_model):
         """
         Apply thermodynamics constraints defined in thermoDB to Mcore & Rcore
@@ -136,6 +104,34 @@ class LumpGEM:
 
         return tfa_model
 
+    def _generate_binary_variables(self):
+        """
+        :return: A dict associating each non-core reaction with a binary variable
+        """
+        return {rxn: BinaryVariable(rxn.id, self._tfa_model) for rxn in self._rncore}
+
+    def _generate_constraints(self):
+        """
+        Generate carbon intake related constraints for each non-core reaction and 
+        growth rate related constraints for each BBB reaction
+        """
+        # Carbon intake constraints
+        for rxn in self._bin_vars.keys():
+            var = self._bin_vars[rxn]
+            constraint = self._tfa_model.problem.Constraint(rxn.forward_variable +
+                                                            rxn.reverse_variable +
+                                                            self._C_uptake * var, ub=self._C_uptake)
+            self._tfa_model.add_cons_vars([var, constraint])
+
+    def _generate_objective(self):
+        """
+        Generate and add the maximization objective
+        """
+        # Sum of binary variables to be maximized
+        objective_sum = sum(self._bin_vars.values())
+        # Set the sum as the objective function
+        self._tfa_model.objective = self._tfa_model.problem.Objective(objective_sum, direction='max')
+
     def run_optimisation(self):
         self._tfa_model.prepare()
 
@@ -148,15 +144,18 @@ class LumpGEM:
         tfa_solution = self._tfa_model.optimize()
         return tfa_solution
 
-    def lump_reaction(self, rBBB):
+    def lump_reaction(self, bio_rxn):
         """
-        :param rBBB: The objective biomass reaction
+        :param bio_rxn: The objective biomass reaction
         :return:
         """
 
-        # Setting the new objective
-        self._model.objective = rBBB
+        constraint = self._tfa_model.problem.Constraint(bio_rxn.flux_expression, lb=self._growth_rate)
+        self._tfa_model.add_cons_vars(constraint)
+
         # Computing TFA solution
         solution = self.run_optimisation()
 
+        self._tfa_model.remove_cons_vars(constraint)
+        
         #TODO : generate lumped reaction
