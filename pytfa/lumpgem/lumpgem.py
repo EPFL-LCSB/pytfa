31a0b1fb2d8759bc851d91a48b4a8b062b7cc266
Benjamin Mouscadet <benjamin.mouscadet@supelec.fr>
2019-03-25 16:28:47 +0100

    FIX : corrected multiple problems with constraints

--- pytfa/lumpgem/lumpgem.py
+++ pytfa/lumpgem/lumpgem.py
@@ -5,8 +5,12 @@ from ..io.base import import_matlab_model, load_thermoDB
 from cobra.io import load_json_model, load_yaml_model, read_sbml_model
 from ..optim.utils import symbol_sum
 
-from ..optim.variables import BinaryVariable
+from pytfa.optim.variables import ReactionVariable, BinaryVariable, get_binary_type
+from pytfa.optim.constraints import ReactionConstraint
 from ..thermo.tmodel import ThermoModel
+
+from pytfa.io import read_compartment_data, apply_compartment_data, \
+                     read_lexicon, annotate_from_lexicon
 
 from numpy import sum
 
@@ -15,11 +19,30 @@ GUROBI = 'optlang-gurobi'
 GLPK = 'optlang-glpk'
 
 
+class MyVariableClass(ReactionVariable, BinaryVariable):
+    prefix = 'VC_'
+
+    def __init__(self, reaction, **kwargs):
+        ReactionVariable.__init__(self, reaction,
+                                  type=get_binary_type(),
+                                  **kwargs)
+
+
+# Define a new constraint type:
+class MyConstraintClass(ReactionConstraint):
+    prefix = 'CC_'
+
+
+# Define a new constraint type:
+class BBBConstraint(MyConstraintClass):
+    prefix = 'BBB_'
+
+
 class LumpGEM:
     """
     A class encapsulating the LumpGEM algorithm
     """
-    def __init__(self, path_to_model, biomass_rxns, core_subsystems, carbon_uptake, growth_rate,  thermo_data_path):
+    def __init__(self, path_to_model, biomass_rxns, core_subsystems, carbon_uptake, growth_rate,  thermo_data_path, auxiliary_data):
         """
         : param GEM: the GEM 
         : type GEM: cobra model
@@ -43,30 +66,31 @@ class LumpGEM:
         # Load the GEM through the appropriate cobra loading function (based on path suffix)
         model = self._load_model(path_to_model)
         # Build thermo model
-        self._tfa_model = self._apply_thermo_constraints(thermo_data_path, model)
+        self._tfa_model = self._apply_thermo_constraints(thermo_data_path, model, auxiliary_data)
 
         # Set containing every BBB reaction
-        self._rBBB = set([])
+        self._rBBB = []
         # Set containing every core reaction
-        self._rcore = set([])
+        self._rcore = []
         # Set containing every core metabolite
-        self._mcore = set([])
+        self._mcore = []
         # Set containing every non-core reaction
-        self._rncore = set([])
+        self._rncore = []
 
         # For each reaction
-        for rxn in model.reactions:
+        for rxn in self._tfa_model.reactions:
             # If it's a BBB reaction
+            # TODO : make it possible to use keywords to match BBB reactions, rather than IDs
             if rxn.id in biomass_rxns:
-                self._rBBB.add(rxn)
+                self._rBBB.append(rxn)
             # If it's a core reaction
-            elif rxn.subsystem in core_subsystems:
-                self._rcore.add(rxn)
+            if rxn.subsystem in core_subsystems:
+                self._rcore.append(rxn)
                 for met in rxn.metabolites:
-                    self._mcore.add(met)
+                    self._mcore.append(met)
             # If it's neither BBB nor core, then it's non-core
             else:
-                self._rncore.add(rxn)
+                self._rncore.append(rxn)
 
         # Carbon uptake
         self._C_uptake = carbon_uptake
@@ -76,12 +100,12 @@ class LumpGEM:
         # TODO : solver choice
         self._solver = 'optlang-cplex'
 
-        self._bin_vars = self._generate_binary_variables()
+        self._activation_vars = self._generate_activation_vars()
         self._generate_constraints()
         self._generate_objective()
 
     def _load_model(self, path_to_model):
-        # Matlab
+        # MATLAB
         if path_to_model[-4:] == ".mat":
             return import_matlab_model(path_to_model)
 
@@ -97,7 +121,7 @@ class LumpGEM:
         if path_to_model[-4:] == ".xml":
             return read_sbml_model(path_to_model)
 
-    def _apply_thermo_constraints(self, thermo_data_path, cobra_model):
+    def _apply_thermo_constraints(self, thermo_data_path, cobra_model, auxiliary_data_path):
         """
         Apply thermodynamics constraints defined in thermoDB to Mcore & Rcore
         """
@@ -105,39 +129,48 @@ class LumpGEM:
         tfa_model = ThermoModel(thermo_data, cobra_model)
         tfa_model.name = 'Lumped Model'
 
-        # TODO : Check what are these operations for
-        # self.read_lexicon = read_lexicon()
-        # compartment_data = read_compartment_data()
-        # annotate_from_lexicon(tfa_model, lexicon)
-        # apply_compartment_data(tfa_model, compartment_data)
+        # TODO : Improve management of auxiliary data paths
+        if auxiliary_data_path[-1] != '/':
+            auxiliary_data_path += '/'
+        lexicon = read_lexicon(auxiliary_data_path+'lexicon.csv')
+        compartment_data = read_compartment_data(auxiliary_data_path+'compartment_data.json')
+
+        annotate_from_lexicon(tfa_model, lexicon)
+        apply_compartment_data(tfa_model, compartment_data)
 
         return tfa_model
 
-    def _generate_binary_variables(self):
-        """
-        :return: A dict associating each non-core reaction with a binary variable
-        """
-        return {rxn: BinaryVariable(rxn.id, self._tfa_model) for rxn in self._rncore}
+    def _generate_activation_vars(self):
+        return {rxn: self._tfa_model.add_variable(kind=MyVariableClass, hook=rxn, lb=0, ub=1, queue=False)
+                for rxn in self._rncore}
 
     def _generate_constraints(self):
         """
         Generate carbon intake related constraints for each non-core reaction
         """
-        # Carbon intake constraints
-        for rxn in self._bin_vars.keys():
-            var = self._bin_vars[rxn]
-            constraint = self._tfa_model.problem.Constraint(rxn.forward_variable +
-                                                            rxn.reverse_variable +
-                                                            self._C_uptake * var, ub=self._C_uptake)
-            # TODO Checkwhether this is the right way to do it
-            self._tfa_model.add_cons_vars([var.variable, constraint])
+        for rxn in self._rncore:
+            activation_var = self._activation_vars[rxn]
+
+            # variable that should be bounded by carbon_uptake
+            reac_var = rxn.forward_variable + rxn.reverse_variable + activation_var * self._C_uptake
+            # adding the constraint to the model
+            self._tfa_model.add_constraint(kind=MyConstraintClass,
+                                           hook=rxn,
+                                           expr=reac_var,
+                                           ub=self._C_uptake,
+                                           queue=True)
+
+        # push constraints in one bulk (faster)
+        self._tfa_model._push_queue()
+        # refresh constraint fields
+        self._tfa_model.repair()
 
     def _generate_objective(self):
         """
         Generate and add the maximization objective
         """
         # Sum of binary variables to be maximized
-        objective_sum = symbol_sum(self._bin_vars.values())
+        objective_sum = symbol_sum(list(self._activation_vars.values()))
         # Set the sum as the objective function
         self._tfa_model.objective = self._tfa_model.problem.Objective(objective_sum, direction='max')
 
@@ -145,6 +178,7 @@ class LumpGEM:
         self._tfa_model.prepare()
 
         # Deactivate tfa computation for non-core reactions
+        #TODO : check whether it must be done once or at each iteration
         for ncrxn in self._rncore:
             ncrxn.thermo['computed'] = False
 
@@ -159,21 +193,33 @@ class LumpGEM:
         :return: the lumped reaction
         """
         # Growth-related constraint
+        #constraint = self._tfa_model.problem.Constraint(bio_rxn.flux_expression, lb=self._growth_rate)
+        #self._tfa_model.add_cons_vars(constraint)
+
         # TODO check lower bound
-        constraint = self._tfa_model.problem.Constraint(bio_rxn.flux_expression, lb=self._growth_rate)
-        self._tfa_model.add_cons_vars(constraint)
+        self._tfa_model.add_constraint(kind=BBBConstraint,
+                                       hook=bio_rxn,
+                                       expr=bio_rxn.flux_expression,
+                                       lb=self._growth_rate,
+                                       queue=False)
 
         # Computing TFA solution
         solution = self.run_optimisation()
 
-        # TODO symbol_sum here ?
-        # TODO use generators to improve speed
+        # TODO use generators to improve speedw
+        # TODO why symbol_sum does not work
         lumped_core_reactions = sum([rxn * solution.fluxes.get(rxn.id) for rxn in self._rcore])
-        lumped_ncore_reactions = sum([rxn * solution.fluxes.get(rxn.id)*self._bin_vars[rxn].Variable.primal for rxn in self._rncore])
+        lumped_ncore_reactions = sum([rxn * solution.fluxes.get(rxn.id)*self._activation_vars[rxn].variable.primal for rxn in self._rncore])
+        lumped_BBB_reactions = sum([rxn * solution.fluxes.get(rxn.id) for rxn in self._rBBB])
 
-        lumped_reaction = sum([lumped_core_reactions, lumped_ncore_reactions])
+        #lumped_core_reactions = symbol_sum([rxn * solution.fluxes.get(rxn.id) for rxn in self._rcore])
+        #lumped_ncore_reactions = symbol_sum([rxn * solution.fluxes.get(rxn.id)*self._activation_vars[rxn].Variable.primal for rxn in self._rncore])
+
+        lumped_reaction = sum([lumped_core_reactions, lumped_ncore_reactions, lumped_BBB_reactions])
 
         # Removing the growth_related constraint to prevent interference with the next lump computation
-        self._tfa_model.remove_cons_vars(constraint)
+        for constraint in self._tfa_model.get_constraints_of_type(BBBConstraint):
+            self._tfa_model.remove_constraint(constraint)
 
-        return lumped_reaction
+        return (lumped_ncore_reactions, lumped_core_reactions, lumped_reaction)
+        #return lumped_reaction
