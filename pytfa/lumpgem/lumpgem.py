d45c6ee23a1cdd2d42d15586c77b95cfaea424ba
Benjamin Mouscadet <benjamin.mouscadet@supelec.fr>
2019-04-24 19:37:33 +0200

    REF : removed temporary model to compute lump

--- pytfa/lumpgem/lumpgem.py
+++ pytfa/lumpgem/lumpgem.py
@@ -15,8 +15,6 @@ from pytfa.io import read_compartment_data, apply_compartment_data, \
 
 from numpy import sum
 
-from tqdm import tqdm
-
 CPLEX = 'optlang-cplex'
 GUROBI = 'optlang-gurobi'
 GLPK = 'optlang-glpk'
@@ -171,35 +169,36 @@ class LumpGEM:
 
     def _prepare_sinks(self):
         all_sinks = {}
-        sink_reactions = []
         print("Preparing sinks...")
 
         for bio_rxn in self._rBBB:
             print(bio_rxn.id)
-            for met, stoech_coeff in tqdm(bio_rxn.metabolites.items()):
+            for met, stoech_coeff in bio_rxn.metabolites.items():
 
-                # stoech_coeff < 0 indicates that the metabolite is a reactant.
+                # stoech_coeff < 0 indicates that the metabolite is a reactant
                 if (stoech_coeff < 0) and (met not in all_sinks.keys()):
                     print("   " + met.id)
                     sink = Reaction("Sink_" + bio_rxn.id + "_" + met.id)
                     sink.name = "Sink_" + bio_rxn.name + "_" + met.name
                     # Subsystem specific to BBB sinks
                     sink.subsystem = "Demand"
-                    # TODO sink.lower_bound = self._growth_rate * stoech_coeff
+
+                    # A sink is simply a reaction which consumes the BBB
                     sink.add_metabolites({met: -1})
+                    # The sinks will be activated later (cf compute_lumps), one at a time
                     sink.knock_out()
 
+                    # The stoechiometric coefficients will be used to define the lower bound of the sink,
+                    # thus it must be stored
                     all_sinks[met] = (sink.id, -stoech_coeff)
-                    sink_reactions.append(sink)
+                    self._tfa_model.add_reactions([sink])
 
                 # reactant already seen
                 elif stoech_coeff < 0:
-                    # TODO check this
+                    # The BBB has already been associated to a sink, so we simply increase the bound of the sink
                     all_sinks[met][1] -= stoech_coeff
                     # Equivalent to this, but there is a knockout :
                     #self._tfa_model.reactions.get_by_id(sinks[met]).lower_bound += self._growth_rate * stoech_coeff
-
-        self._tfa_model.add_reactions(sink_reactions)
 
         self._tfa_model.prepare()
         for ncrxn in self._rncore:
@@ -217,23 +216,33 @@ class LumpGEM:
         self._tfa_model.objective = self._tfa_model.problem.Objective(objective_sum, direction='max')
 
     def compute_lumps(self):
+
+        # Must be called before optimization
+        self._tfa_model.convert()
+
+        # dict: {metabolite: lumped_reaction}
         lumps = {}
-        for met_BBB, (sink_id, stoech_coeff) in tqdm(self._sinks.items()):
-            print("Considering " + met_BBB.id)
-            with self._tfa_model as model:
-                model.reactions.get_by_id(sink_id).lower_bound = self._growth_rate * stoech_coeff
-                model.convert()
-                tfa_solution = model.optimize()
 
-                # TODO use generators to improve speed
-                # TODO maybe use sympy.add
-                lumped_core_reactions  = sum([rxn * tfa_solution.fluxes.get(rxn.id) for rxn in self._rcore])
-                lumped_ncore_reactions = sum([rxn * tfa_solution.fluxes.get(rxn.id) * self._activation_vars[rxn].variable.primal for rxn in self._rncore])
-                lumped_BBB_reactions   = sum([rxn * tfa_solution.fluxes.get(rxn.id) for rxn in self._rBBB])
+        for met_BBB, (sink_id, stoech_coeff) in self._sinks.items():
+            print("Considering :" + met_BBB.id)
 
-                lumped_reaction = sum([lumped_core_reactions, lumped_ncore_reactions, lumped_BBB_reactions])
+            # Activate reaction by setting its lower bound
+            self._tfa_model.reactions.get_by_id(sink_id).lower_bound = self._growth_rate * stoech_coeff
 
-                lumps[met_BBB] = lumped_reaction
+            # TODO timeout
+            tfa_solution = self._tfa_model.optimize()
+
+            # TODO maybe use sympy.add
+            lumped_core_reactions  = sum([rxn * tfa_solution.fluxes.get(rxn.id) for rxn in self._rcore])
+            lumped_ncore_reactions = sum([rxn * tfa_solution.fluxes.get(rxn.id) * self._activation_vars[rxn].variable.primal for rxn in self._rncore])
+            lumped_BBB_reactions   = sum([rxn * tfa_solution.fluxes.get(rxn.id) for rxn in self._rBBB])
+
+            lumped_reaction = sum([lumped_core_reactions, lumped_ncore_reactions, lumped_BBB_reactions])
+
+            lumps[met_BBB] = lumped_reaction
+
+            # Deactivating reaction by setting both bounds to 0
+            self._tfa_model.reactions.get_by_id(sink_id).knock_out()
 
         return lumps
 
