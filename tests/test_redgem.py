5502a0be6f419990b95480c5d92d0bc594783c80
Romain Poirot <romain.poirot@supelec.fr>
2019-05-02 18:47:22 +0100

    FIX: Remove unused parameters in test script

--- tests/test_redgem.py
+++ tests/test_redgem.py
@@ -6,16 +6,15 @@ from pytfa.thermo.tmodel import ThermoModel
 from pytfa.io import  read_compartment_data, apply_compartment_data, read_lexicon, annotate_from_lexicon
 
 
-
 path_to_model = 'models/small_ecoli.mat'
 thermoDB = "data/thermo_data.thermodb"
-carbon_uptake = 60000
-growth = 0.5
+path_to_lexicon = 'models/iJO1366/lexicon.csv'
+path_to_compartment_data = 'models/iJO1366/compartment_data.json'
 
 model = import_matlab_model(path_to_model)
 thermo_data = load_thermoDB(thermoDB)
-lexicon = read_lexicon('models/iJO1366/lexicon.csv')
-compartment_data = read_compartment_data('models/iJO1366/compartment_data.json')
+lexicon = read_lexicon(path_to_lexicon)
+compartment_data = read_compartment_data(path_to_compartment_data)
 
 tfa_model = ThermoModel(thermo_data, model)
 annotate_from_lexicon(tfa_model, lexicon)
