09132d4b3c25aa86f6f5e5e1bc5b68d18848708f
carrascomj <carrascomurielj@gmail.com>
2019-12-17 12:48:57 +0100

    FIX: test equilibrator compatibility only for >= py3.6

--- tests/test_equilibrator.py
+++ tests/test_equilibrator.py
@@ -1,10 +1,14 @@
 """Tests for the equilibrator integration."""
 
 import os
+import pytest
 import pytfa
 import pytfa.io
+import sys
 
-from pytfa.thermo.equilibrator import build_thermo_from_equilibrator
+equi = pytest.importorskip(
+    "pytfa.thermo.equilibrator"
+)
 
 this_directory = os.path.dirname(os.path.realpath(__file__))
 
@@ -16,6 +20,10 @@ cobra_model.optimizer = "glpk"
 tmodel = None
 
 
+# previous version aren't compatible with the equlibrator_cache version needed.
+# this annotation is not necessary since the importorskip statement should make
+# the tests fail before, but this way it is more explicit
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires >= python3.6")
 def test_load_with_equi_thermo():
     """Build thermo data structure with equilibrator."""
     global tmodel
@@ -23,23 +31,26 @@ def test_load_with_equi_thermo():
     for met in cobra_model.metabolites:
         # normalize but don't overwrite
         met.annotation["seed.compound"] = met.annotation["seed_id"]
-    thermo_data = build_thermo_from_equilibrator(cobra_model)
+    thermo_data = equi.build_thermo_from_equilibrator(cobra_model)
     tmodel = pytfa.ThermoModel(thermo_data, cobra_model)
     tmodel.solver = 'optlang-glpk'
 
 
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires >= python3.6")
 def test_equilibrator_preparation():
     """Prepare using equilibrator_api."""
     global tmodel
     tmodel.prepare()
 
 
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires >= python3.6")
 def test_equilibrator_conversion():
     """Convert by the usual method in `ThermoModel`."""
     global tmodel
     tmodel.convert()
 
 
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires >= python3.6")
 def test_equilibrator_optim():
     """LP optimization by the usual method in `ThermoModel`."""
     global tmodel
