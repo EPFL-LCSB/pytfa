d530ac773b0e4a5bcd81b11ea57d57d81ed9af3c
carrascomj <carrascomurielj@gmail.com>
2019-12-20 12:55:28 +0100

    fix: import on function

--- tests/test_equilibrator.py
+++ tests/test_equilibrator.py
@@ -2,19 +2,16 @@
 # -*- coding: utf-8 -*-
 """Tests for the equilibrator integration."""
 import os
+from pytfa import ThermoModel
 import pytest
-import pytfa
-import pytfa.io
 import sys
 
-from pytfa.thermo.equilibrator import build_thermo_from_equilibrator
+from settings import cobra_model
+
 
 this_directory = os.path.dirname(os.path.realpath(__file__))
 
 # Load the cobra_model
-cobra_model = pytfa.io.import_matlab_model(
-    this_directory + "/../models/small_ecoli.mat"
-)
 cobra_model.optimizer = "glpk"
 tmodel = None
 
@@ -25,14 +22,16 @@ tmodel = None
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="requires >= python3.6")
 def test_load_with_equi_thermo():
     """Build thermo data structure with equilibrator."""
+    from pytfa.thermo.equilibrator import build_thermo_from_equilibrator
+
     global tmodel
     global cobra_model
     for met in cobra_model.metabolites:
         # normalize but don't overwrite
         met.annotation["seed.compound"] = met.annotation["seed_id"]
     thermo_data = build_thermo_from_equilibrator(cobra_model)
-    tmodel = pytfa.ThermoModel(thermo_data, cobra_model)
-    tmodel.solver = 'optlang-glpk'
+    tmodel = ThermoModel(thermo_data, cobra_model)
+    tmodel.solver = "optlang-glpk"
 
 
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="requires >= python3.6")
