9de062e6d3ae82b3aef88e11ed3337fff1459bb3
Pierre Salvy <pierre.salvy@epfl.ch>
2020-02-10 10:11:51 +0100

    ENH: Moved linearization tools from etfl to here

--- pytfa/optim/variables.py
+++ pytfa/optim/variables.py
@@ -555,3 +555,10 @@ class NegSlackLC(MetaboliteVariable):
 
     prefix = 'NegSlackLC_'
 
+
+class LinearizationVariable(ModelVariable):
+    """
+    Class to represent a product A*B when performing linearization of the
+    model
+    """
+    prefix = 'LZ_'
